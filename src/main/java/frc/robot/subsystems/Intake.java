8e3cf2ac2ba76ad566530a262a8752f157d0f512
ojlennon <45679125+ojlennon@users.noreply.github.com>
2023-11-29 08:53:57 -0700

    non voltage

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -12,6 +12,7 @@ public class Intake extends SubsystemBase {
     private CCSparkMax intake1 = new CCSparkMax("Intake One", "in1", RobotMap.INTAKE_1, MotorType.kBrushless, IdleMode.kBrake, RobotMap.INTAKE_1_REVERSED);
     private CCSparkMax intake2 = new CCSparkMax("Intake Two", "in2", RobotMap.INTAKE_2, MotorType.kBrushless, IdleMode.kBrake, RobotMap.INTAKE_2_REVERSED);
     MotorControllerGroup intake = new MotorControllerGroup(intake1, intake2);
+
     public void spin(double speed) {
         intake.set(speed);
     }
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -25,7 +25,6 @@ public class SwerveModule extends SubsystemBase {
     private CCSparkMax turnMotor;
 
     private PIDController turningPIDController, drivingPidController;
-    private SimpleMotorFeedforward driveVelocityFeedforward;
 
     private AnalogEncoder absoluteEncoder;
     private double absoluteEncoderOffset;
@@ -141,13 +140,8 @@ public class SwerveModule extends SubsystemBase {
         state = SwerveModuleState.optimize(state, getState().angle);
         //integrate max speed here
             // isOpenLoop would be true in teleop perhaps because some drivers, like ours prefers it that way
-        double feedforwardVoltage = driveVelocityFeedforward.calculate(state.speedMetersPerSecond);
-        if (isOpenLoop) {
-            // Only using feedfoward here, no PID because in teleop we don't really need to correct it
-            driveMotor.setVoltage(feedforwardVoltage);
-        } else {
-            driveMotor.setVoltage(feedforwardVoltage + drivingPidController.calculate(getDriveVelocity(), state.speedMetersPerSecond);
-        }
+       
+        driveMotor.set(state.speedMetersPerSecond);
         turnMotor.set(turningPIDController.calculate(getTurnPosition(), state.angle.getRadians()));
     }
 
